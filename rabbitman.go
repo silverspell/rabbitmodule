18c07528058ba211aa3989412400ce2455a3249c
Cem G <cem@marti.tech>
2021-09-01 18:26:08 +0300

    v1.0.5

--- rabbitman.go
+++ rabbitman.go
@@ -34,14 +34,14 @@ func exchangeDeclare(ch *amqp.Channel, excType, exchange string) error {
 	)
 }
 
-func declareQueue(ch *amqp.Channel) (amqp.Queue, error) {
+func declareQueue(ch *amqp.Channel, queueName string) (amqp.Queue, error) {
 	return ch.QueueDeclare(
-		"",    // name
-		false, // durable
-		false, // delete when unused
-		true,  // exclusive
-		false, // no-wait
-		nil,   // arguments
+		queueName, // name
+		true,      // durable
+		false,     // delete when unused
+		true,      // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
 }
 
@@ -59,7 +59,7 @@ func consume(ch *amqp.Channel, queueName string) (<-chan amqp.Delivery, error) {
 	return ch.Consume(
 		queueName,
 		"",
-		true,
+		false,
 		false,
 		false,
 		false,
@@ -88,7 +88,7 @@ func ConnectSubscriber(reply chan string, exchange string) {
 	defer ch.Close()
 	err = exchangeDeclare(ch, "fanout", exchange)
 	failOnError(err, "[SUBSCRIBER] failed to declare exchange")
-	q, err := declareQueue(ch)
+	q, err := declareQueue(ch, "")
 	failOnError(err, "[SUBSCRIBER] failed to declare queue")
 	err = bindQueue(ch, q.Name, exchange, "")
 	failOnError(err, "[SUBSCRIBER] Fail to bind")
@@ -99,6 +99,7 @@ func ConnectSubscriber(reply chan string, exchange string) {
 		for d := range msgs {
 			fmt.Printf("[SUBSCRIBE] %s\n", string(d.Body))
 			reply <- string(d.Body)
+			d.Ack(false)
 		}
 	}()
 
@@ -139,7 +140,7 @@ func ConnectSubscriberDirect(reply chan string, exchange, routeKey string) {
 	defer ch.Close()
 	err = exchangeDeclare(ch, "direct", exchange)
 	failOnError(err, "[SUBSCRIBER] failed to declare exchange")
-	q, err := declareQueue(ch)
+	q, err := declareQueue(ch, "")
 	failOnError(err, "[SUBSCRIBER] failed to declare queue")
 	err = bindQueue(ch, q.Name, exchange, routeKey)
 	failOnError(err, "[SUBSCRIBER] Fail to bind")
@@ -150,6 +151,7 @@ func ConnectSubscriberDirect(reply chan string, exchange, routeKey string) {
 		for d := range msgs {
 			fmt.Printf("[SUBSCRIBE] %s\n", string(d.Body))
 			reply <- string(d.Body)
+			d.Ack(false)
 		}
 	}()
 
@@ -179,5 +181,53 @@ func ConnectPublisherDirect(listen chan string, exchange, routeKey string) {
 			fmt.Printf("[x] Sent %s\n", msg)
 		}
 	}
+}
 
+func ConnectSubscriberTaskQueue(reply chan string, queueName string) {
+	conn, err := connect()
+	failOnError(err, "[SUBSCRIBER] Failed to connect")
+	defer conn.Close()
+	ch, _ := conn.Channel()
+	defer ch.Close()
+	q, err := declareQueue(ch, queueName)
+	failOnError(err, "[SUBSCRIBER] failed to declare queue")
+	err = ch.Qos(1, 0, false) //prefetch count, prefetch size
+	failOnError(err, "[SUSBCRIBER] failed to set qos")
+	msgs, _ := consume(ch, q.Name)
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			fmt.Printf("[SUBSCRIBE] %s\n", string(d.Body))
+			reply <- string(d.Body)
+			d.Ack(false)
+		}
+	}()
+
+	fmt.Println("[RABBITMAN] Waiting for messages")
+	<-forever
+	fmt.Println("[RABBITMAN] Exiting?")
+}
+
+func ConnectPublisherTaskQueue(listen chan string, queueName string) {
+	conn, err := connect()
+	failOnError(err, "[PUBLISHER] Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "[PUBLISHER] Failed to open a channel")
+	defer ch.Close()
+
+	q, err := declareQueue(ch, queueName)
+	failOnError(err, "[PUBLISHER] failed to declare Queue")
+	for {
+		msg := <-listen
+		err = publish(ch, "", msg, q.Name)
+		failOnError(err, "[PUBLISHER] Failed to publish a message")
+
+		if getEnv("RABBIT_ENV") != "" {
+			fmt.Printf("[x] Sent %s\n", msg)
+		}
+	}
 }
